621499c0f779e869c9587e379487bed65a7a6e4e
sbn317783 <000100925@uca.edu.sv>
2026-09-10 12:14:53 -0600

    Con este push he sube la feature de Ingresar Stock, la cual permite al usuario el ingresar el stock en su inventario

--- inventario.cc
+++ inventario.cc
@@ -1,7 +1,42 @@
 #include <iostream>
 
+
+void IngresarStock(int &stock, int cantidad); 
+
+struct Stock{
+    int cantidad;
+};
+
+
+
+
 int main()
 {
+   int stock = 50; 
+   std:: cout << "======INICIO DE PROGRAMA =====" << std:: endl; 
+   std:: cout << "Inicio del stock asignado: " << stock << "unidades \n"<<  std:: endl; 
+
+   //Prueba para ver comprobar IngresarStock
+   IngresarStock(stock, 25);
+   
+
+
+
+
+
     
     return 0;
+}
+
+void IngresarStock(int &stock, int cantidad)
+{
+    if (cantidad < 0 )
+    {
+        std:: cout << "No se permite ingresar cantidades negativas" << std:: endl; 
+    }
+    else{
+        stock += cantidad; 
+        std:: cout << "Exito! se han ingresado " << cantidad << " De productos Correctamente" << std:: endl; 
+    }
+    
 }
