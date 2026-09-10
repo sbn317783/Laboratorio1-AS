f2944cb9d2fe613e35b9cd8cf3f2ff26903ba414
jaem25 <00018026@uca.edu.sv>
2026-09-10 12:08:55 -0600

    feat: Agregando funciones Retirar y Consultar Stock

--- inventario.cc
+++ inventario.cc
@@ -1,7 +1,41 @@
 #include <iostream>
 
+struct Almacen
+{
+    int stock;
+};
+
+
+void RetirarStock(int *stock, int cantidad);
+void ConsultarStock(int stock);
+
+
 int main()
 {
     
     return 0;
+}
+
+
+void RetirarStock(int *stock, int cantidad)
+{
+    if (cantidad > *stock)
+    {
+        std::cout<< "No hay suficiente stock disponible..." <<std::endl;
+    }
+    else if (cantidad <= 0)
+    {
+        std::cout<< "Cantidad invalida para retirar..." <<std::endl;
+    }
+    
+    else
+    {
+        *stock -= cantidad;
+    }
+    
+}
+
+void ConsultarStock(int stock)
+{
+    std::cout<< "Stock actual: " << stock << "unidades" <<std::endl;
 }
