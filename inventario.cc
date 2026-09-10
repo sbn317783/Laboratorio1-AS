25c2b83c65588a1011753951c420c285c13e71af
jaem25 <00018026@uca.edu.sv>
2026-09-10 12:46:19 -0600

    fix: corrigiendo errores

--- inventario.cc
+++ inventario.cc
@@ -16,13 +16,21 @@ void ConsultarStock(int stock);
 
 int main()
 {
-   int stock = 50; 
-   std:: cout << "======INICIO DE PROGRAMA =====" << std:: endl; 
-   std:: cout << "Inicio del stock asignado: " << stock << "unidades \n"<<  std:: endl; 
+    Almacen almacen;
 
-   //Prueba para ver comprobar IngresarStock
-   IngresarStock(stock, 25);
-   
+    almacen.stock = 50; 
+    std:: cout << "======INICIO DE PROGRAMA =====" << std:: endl; 
+    std:: cout << "Inicio del stock asignado: " <<  std:: endl; 
+    ConsultarStock(almacen.stock);
+
+    IngresarStock(almacen.stock, 25);
+    std::cout << "Despues de cargar 25 unidades: " << std::endl;
+    ConsultarStock(almacen.stock);
+
+    RetirarStock(&almacen.stock, 10);
+    std::cout << "Despues de consumir 10 unidades: " << std::endl;
+    ConsultarStock(almacen.stock);
+
 
 
 
@@ -36,14 +44,14 @@ void IngresarStock(int &stock, int cantidad)
 {
     if (cantidad < 0 )
     {
-        std:: cout << "No se permite ingresar cantidades negativas" << std:: endl; 
+        std:: cout << "No se permite ingresar cantidades negativas " << std:: endl; 
     }
     else{
         stock += cantidad; 
         std:: cout << "Exito! se han ingresado " << cantidad << " De productos Correctamente" << std:: endl; 
     }
     
-
+}
 void RetirarStock(int *stock, int cantidad)
 {
     if (cantidad > *stock)
@@ -64,5 +72,5 @@ void RetirarStock(int *stock, int cantidad)
 
 void ConsultarStock(int stock)
 {
-    std::cout<< "Stock actual: " << stock << "unidades" <<std::endl;
+    std::cout<< "Stock actual: " << stock << " unidades" <<std::endl;
 }
